33121256ade93bb26f69c69714ebfb3043d597ea
betterKaren <dupn_1025@163.com>
2019-11-16 18:26:27 -0500

    prelab

--- lab9/prelab/threexinput.cpp
+++ lab9/prelab/threexinput.cpp
@@ -11,26 +11,28 @@ extern "C" int threexplusone(int);
 
 int  main () {
     // delcare the local variables
-    int x, n, count;
+    int x, n, count, i;
     double time;
 
     cout << "Please enter a possitive integer as x:";
     cin >> x;
     cout << "Please enter a positive integer as n:";
     cin >> n;
+    i = n;
 
     // call externally defined functions to get the result
     // start timer
     timer t;
     t.start();
-    while(n-- > 0) {
+    while(i-- > 0) {
 	    count = threexplusone(x);
     }
     t.stop();
-    time = t.getTime()*1000;
+    time = t.getTime()*1000000;
+    double avg = time/n;
 
 	cout << x << " took " << count << " steps to reach the value 1 " << endl;
-	cout << "Average time = " << time/n << "ms" << endl;
+	cout << "Average time on " << n << " runs is "<< avg << "us" << endl;
     // no need to deallocate
     return 0;
 }
