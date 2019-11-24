3c99d4adfb7a82114cc9b88a55a1f50ac574a975
betterKaren <dupn_1025@163.com>
2019-11-24 16:27:07 -0500

    lab9 inlab&postlab

---
+++ lab9/inlab/dynamicDispatch.cpp
@@ -0,0 +1,26 @@
+﻿// Type your code here, or load an example.
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Person {
+    public:
+        virtual void name(string &n);
+        virtual void age(int &a);
+};
+
+class Student: public Person{
+    public:
+        virtual void name(string &n);
+        virtual void age(int &a);
+        virtual void year(int &y);
+};
+
+int main() {
+    Person *p = new Student();
+    string name = "yd";
+    p->name(name);
+    int age = 20;
+    p->age(age);
+    return 0;
+}
---
+++ lab9/postlab/inheritance.cpp
@@ -0,0 +1,45 @@
+﻿// Type your code here, or load an example.
+#include <string>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class Person {
+    public:
+        Person(void) : myName("") {}
+        ~Person(void) {}
+        void setName(string newName) {
+            myName = newName;
+        }
+        void print() {
+            cout << myName << endl;
+        }
+    private:
+        string myName;
+};
+
+class Student: public Person {
+    public:
+        Student(void) {
+            myDept = "";
+        }
+        ~Student(void) {}
+        void setDept(string newDept) {
+            myDept = newDept;
+        }
+        void print() {
+            Person::print();
+            cout << myDept << endl;
+        }
+    private:
+        string myDept;
+};
+
+int main() {
+    Student s;
+    s.setDept("CS");
+    s.print();
+    s.setName("yd");
+    s.print();
+    return 0;
+}
